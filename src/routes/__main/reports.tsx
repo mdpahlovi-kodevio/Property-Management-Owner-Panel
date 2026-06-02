662a92a58c87249f449ae395bf081c13590b67e0
Rakesh Karmaker <rakeshkarmaker65@gmail.com>
2026-06-02 19:17:10 +0600

    Feat: Null

--- src/routes/__main/reports.tsx
+++ src/routes/__main/reports.tsx
@@ -176,7 +176,6 @@ function RouteComponent() {
                     Filter
                 </Button>
             </div>
-
             {activeTab === 'occupancy' && (
                 <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4">
                     <StatCard
