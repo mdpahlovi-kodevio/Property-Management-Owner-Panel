40e3db675971bb94df4c788f685ee7575bbdad9e
Rakesh Karmaker <rakeshkarmaker65@gmail.com>
2026-06-03 17:07:51 +0600

    Feat: Hover effect on table data row.

--- src/components/ui/table.tsx
+++ src/components/ui/table.tsx
@@ -25,7 +25,7 @@ function TableFooter({ className, ...props }: React.ComponentProps<'tfoot'>) {
 }
 
 function TableRow({ className, ...props }: React.ComponentProps<'tr'>) {
-    return <tr data-slot="table-row" className={className} {...props} />
+    return <tr data-slot="table-row" className={cn("hover:bg-primary/5 data-[state=selected]:bg-muted transition-colors", className)} {...props} />
 }
 
 function TableHead({ className, ...props }: React.ComponentProps<'th'>) {
